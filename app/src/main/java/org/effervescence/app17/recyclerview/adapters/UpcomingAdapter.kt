701c74ad32c0fcef2df494e28de3d1bc60db3bf3
nikhilmundra29 <nikhilmundra29@gmail.com>
2017-10-01 19:37:37 +0530

    Event images loaded

--- app/src/main/java/org/effervescence/app17/recyclerview/adapters/UpcomingAdapter.kt
+++ app/src/main/java/org/effervescence/app17/recyclerview/adapters/UpcomingAdapter.kt
@@ -5,14 +5,20 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.upcoming_event_layout.view.*
 import org.effervescence.app17.R
 import org.effervescence.app17.models.Event
 import java.util.*
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
+import com.bumptech.glide.module.AppGlideModule
+import com.squareup.picasso.Picasso
+
 
 /**
  * Created by betterclever on 17/09/17.
  */
+
 class UpcomingAdapter(val context: Context) : RecyclerView.Adapter<UpcomingAdapter.ViewHolder>() {
 
     private val events = ArrayList<Event>()
@@ -24,7 +30,7 @@ class UpcomingAdapter(val context: Context) : RecyclerView.Adapter<UpcomingAdapt
                     .inflate(R.layout.upcoming_event_layout,parent,false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItem(events[position])
+        holder.bindItem(context, events[position])
     }
 
     fun addEvents(events: List<Event>){
@@ -33,10 +39,17 @@ class UpcomingAdapter(val context: Context) : RecyclerView.Adapter<UpcomingAdapt
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindItem(event: Event) {
+        fun bindItem(context: Context, event: Event) {
             itemView.eventNameTV.text = event.name
             itemView.eventTime.text = Date(event.timestamp).toString().substring(4,16)
             // TODO: Set image Glide
+            if(event.imageUrl.isEmpty())
+                itemView.eventImageView.setImageResource(R.drawable.placeholder_event);
+            else
+                Picasso.with(context).load(event.imageUrl).placeholder(R.drawable.placeholder_event).into(itemView.eventImageView);
+
+
         }
+
     }
 }
