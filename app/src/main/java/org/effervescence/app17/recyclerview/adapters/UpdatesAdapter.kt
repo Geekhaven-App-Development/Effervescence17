655379dd0d9ac22d2d72175026e057b1d0ab66a4
Akshat Jain <lit2016016@iiita.ac.in>
2017-10-06 01:18:21 +0530

    updates

--- app/src/main/java/org/effervescence/app17/recyclerview/adapters/UpdatesAdapter.kt
+++ app/src/main/java/org/effervescence/app17/recyclerview/adapters/UpdatesAdapter.kt
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_event_detail.*
 import kotlinx.android.synthetic.main.updates_layout.view.*
 import org.effervescence.app17.R
 import org.effervescence.app17.models.Notification
